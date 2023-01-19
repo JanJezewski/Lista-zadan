037a971a95e85da1fa74211013be90372a358355
Jan Jeżewski <Jeewski533@gmail.com>
2023-01-19 02:13:10 +0100

    target page appearance

--- first-project/src/app/app.component.ts
+++ first-project/src/app/app.component.ts
@@ -8,6 +8,8 @@ import { Task } from './task';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+[x: string]: any;
+  editMode = false;
  
   title(title: any) {
     throw new Error('Method not implemented.');
@@ -33,24 +35,46 @@ export class AppComponent {
      this.config = {
       title: 'Lista zadań' ,
       task: 'Przykładowe zadanie - 01.01 Niedziela' ,
-      occurtask: 'zrobione' ,
+      
       timecomunicat: 'Aktualna godzina: ' ,
       footer: ' © Lista zadań, All rights reserved.',
-      date: new Date().toDateString(),
+      date: new Date(),
     }
   
   }
   clearTasks() {
     this.tasks = [];
-     }
-     createTask() {
-      const task: Task = {
-        name: this.taskName,
-        deadline: this.taskDate,
-        done: false,
-      };
-      this.tasks.push(task);
-      this.taskName = '';
-      this.taskDate = '';
-    }
+  }
+
+  createTask() {
+    const task: Task = {
+      name: this.taskName,
+      deadline: this.taskDate,
+      done: false,
+    };
+    this.tasks.push(task);
+    this.taskName = '';
+    this.taskDate = '';
+    this.sortTasks();
+  }
+
+  switchEditMode(){
+    this.editMode = !this.editMode;
+  }
+
+  markTaskAsDone(task: Task) {
+    task.done = true;
+    this.sortTasks();
+  }
+
+  deleteTask(task: Task) {
+    this.tasks = this.tasks.filter(e => e !== task);
+    this.sortTasks();
+  }
+
+  private sortTasks() {
+    this.tasks = this.tasks.sort((a: Task, b: Task) =>
+      a.done === b.done ? 0 : a.done ? 1 : -1
+    );
+  }
 }
